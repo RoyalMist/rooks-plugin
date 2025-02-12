a2af62ca4435a96b01d157033756c3b90be5e3a2
Thibault Fouache <thibault.fouache@gmail.com>
2025-02-12 11:17:02 +0100

    Update serde version and add new API integration
    Upgraded serde to version 1.0.217 for better compatibility. Added a new function to fetch random jokes from the Chuck Norris API and updated the plugin to include this feature in the output structure. Adjusted input/output structs and plugin logic accordingly.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,47 @@
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
 
+fn fact() -> FnResult<String> {
+    #[derive(Serialize, Deserialize)]
+    struct Fact {
+        pub icon_url: String,
+        pub id: String,
+        pub url: String,
+        pub value: String,
+    }
+
+    let request = HttpRequest::new("https://api.chucknorris.io/jokes/random").with_method("GET");
+    let res = http::request::<()>(&request, None)?;
+    let fact = res.json::<Fact>()?;
+    Ok(fact.value)
+}
+
 #[derive(FromBytes, ToBytes, Deserialize, Serialize, PartialEq, Debug)]
 #[encoding(Json)]
-struct Data {
+struct Input {
     name: String,
     age: i32,
     happy: bool,
 }
 
+#[derive(FromBytes, ToBytes, Deserialize, Serialize, PartialEq, Debug)]
+#[encoding(Json)]
+struct Output {
+    name: String,
+    age: i32,
+    happy: bool,
+    fact: String,
+}
+
 #[plugin_fn]
-pub fn redact(input: Data) -> FnResult<Data> {
+pub fn call(input: Input) -> FnResult<Output> {
+    info!("Hello from Plugin!");
     let redact = config::get("redact").expect("redact not found");
-    Ok(Data {
-        name: redact.unwrap(),
+
+    Ok(Output {
+        name: format!("New value: {}", redact.unwrap()),
         age: input.age,
         happy: input.happy,
+        fact: fact()?,
     })
 }
